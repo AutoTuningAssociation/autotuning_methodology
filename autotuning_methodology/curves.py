Improved split times calculation, fixed registration of time and performance per key

autotuning_methodology/curves.py
@@ -23,8 +23,8 @@ def get_indices_in_distribution(draws: np.ndarray, dist: np.ndarray, sorter=None
     # check whether each value of draws (excluding NaN) is in dist
     if not skip_draws_check:
         assert np.all(
-            np.in1d(draws[~np.isnan(draws)],
-                    dist)), f"Each value in draws should be in dist, but these are missing: {draws[~np.isnan(draws)][~np.in1d(draws[~np.isnan(draws)], dist)]}"
+            np.in1d(draws[~np.isnan(draws)], dist)
+        ), f"Each value in draws should be in dist, but {np.size(draws[~np.isnan(draws)][~np.in1d(draws[~np.isnan(draws)], dist)])} values of the {np.size(draws)} are missing: {draws[~np.isnan(draws)][~np.in1d(draws[~np.isnan(draws)], dist)]}"
 
     # check the sorter
     if sorter is not None:
@@ -75,9 +75,9 @@ class Curve(ABC):
         results = results_description.get_results()
         self._x_fevals = results.fevals_results    # the time per objective value in number of function evaluations since start (1d if deterministic, 2d if stochastic)
         self._x_time = results.objective_time_results    # the time per objective value in seconds since start the raw x-axis (1d if deterministic, 2d if stochastic)
+        self._x_time_per_key = results.objective_time_results_per_key    # the time per objective time key (2d if deterministic, 3d if stochastic)
         self._y = results.objective_performance_best_results    # the objective performances (1d if deterministic, 2d if stochastic)
-        self._x_time_per_key = results.objective_time_results_per_key
-        self._y_per_key = results.objective_performance_results_per_key
+        self._y_per_key = results.objective_performance_results_per_key    # the performance per objective time key (2d if deterministic, 3d if stochastic)
 
         # complete initialisation
         self.check_attributes()
@@ -94,20 +94,26 @@ class Curve(ABC):
         assert isinstance(self.stochastic, bool)
         assert isinstance(self._x_fevals, np.ndarray)
         assert isinstance(self._x_time, np.ndarray)
+        assert isinstance(self._x_time_per_key, np.ndarray)
         assert isinstance(self._y, np.ndarray)
+        assert isinstance(self._y_per_key, np.ndarray)
 
         # assert values
         if self.stochastic is False:
             assert self._x_fevals.ndim == 1
             assert self._x_fevals[0] == 1    # the first function evaluation must be 1
             assert self._x_time.ndim == 1
+            assert self._x_time_per_key.ndim == 2
             assert self._y.ndim == 1
+            assert self._y_per_key.ndim == 2
         else:
             assert self._x_fevals.ndim == 2
             assert all(self._x_fevals[0] == 1)    # the first function evaluation must be 1
             assert self._x_time.ndim == 2
+            assert self._x_time_per_key.ndim == 3
             assert self._y.ndim == 2
-        assert self._x_fevals.shape == self._x_time.shape == self._y.shape
+            assert self._y_per_key.ndim == 3
+        assert self._x_fevals.shape == self._x_time.shape == self._x_time_per_key.shape[1:] == self._y.shape == self._y_per_key.shape[1:]
 
     @abstractmethod
     def get_curve(self, range: np.ndarray, x_type: str, dist: np.ndarray = None, confidence_level: float = None):
@@ -323,19 +329,24 @@ class StochasticOptimizationAlgorithm(Curve):
     def get_curve(self, range: np.ndarray, x_type: str, dist: np.ndarray = None, confidence_level: float = None):
         return super().get_curve(range, x_type, dist, confidence_level)
 
-    def _get_curve_over_fevals_values_in_range(self, fevals_range: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
-        """Get the valid fevals and values that are in the given range"""
+    def _get_matching_feval_indices_in_range(self, fevals_range: np.ndarray) -> np.ndarray:
+        """ Get a mask of where the fevals range matches with the data """
         assert fevals_range.ndim == 1
         assert np.all(np.isfinite(fevals_range))
+        matching_indices_mask = np.array([np.isin(x_column, fevals_range, assume_unique=True)
+                                          for x_column in self._x_fevals.T]).transpose()    # get the indices of the matching feval range per repeat (column)
+        if np.all(~matching_indices_mask):
+            raise ValueError(f"No overlap in data and given {fevals_range=}")
+        return matching_indices_mask
+
+    def _get_curve_over_fevals_values_in_range(self, fevals_range: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+        """Get the valid fevals and values that are in the given range"""
         target_index: int = fevals_range[-1] - 1
 
         # filter to only get data in the fevals range
-        matching_indices_mask = np.array([np.isin(x_column, fevals_range, assume_unique=True)
-                                          for x_column in self._x_fevals.T]).transpose()    # get the indices of the matching feval range per repeat (column)
+        matching_indices_mask = self._get_matching_feval_indices_in_range(fevals_range)    # get the indices of the matching feval range per repeat (column)
         masked_values = np.where(matching_indices_mask, self._y, np.nan)    # apply the mask to the values, filling NaN for False
         masked_fevals = np.where(matching_indices_mask, self._x_fevals, np.nan).transpose()    # apply the mask to the fevals, filling NaN for False
-        if np.all(~matching_indices_mask):
-            raise ValueError(f"No overlap in data and given {fevals_range=}")
 
         # make sure that the filtered fevals are consistent (every repeat has the same array of fevals)
         if not np.allclose(masked_fevals, masked_fevals[0], equal_nan=True):
@@ -461,7 +472,7 @@ class StochasticOptimizationAlgorithm(Curve):
         times: np.ndarray = times[nan_mask].reshape(-1, num_repeats)
         values: np.ndarray = values[nan_mask].reshape(-1, num_repeats)
 
-        # get the highest time of each repeat, take the median
+        # get the highest time of each run of the algorithm, take the median
         times_no_nan = times
         times_no_nan[np.isnan(values)] = np.nan    # to count only valid configurations towards highest time
         highest_time_per_repeat = np.nanmax(times_no_nan, axis=0)
@@ -624,31 +635,49 @@ class StochasticOptimizationAlgorithm(Curve):
         return super().get_split_times(range, x_type, searchspace_stats)
 
     def get_split_times_at_feval(self, fevals_range: np.ndarray, searchspace_stats: SearchspaceStatistics) -> np.ndarray:
-        fevals, masked_values = self._get_curve_over_fevals_values_in_range(fevals_range)
-        indices = get_indices_in_array(masked_values, searchspace_stats.objective_performances_total)
-        average_index_at_feval = np.array(np.round(np.nanmedian(indices, axis=1)), dtype=int)
-
-        # for each time key, obtain the time at the fevals in fevals_range
+        # get the indices in the range
+        matching_indices_mask = self._get_matching_feval_indices_in_range(fevals_range)
         objective_time_keys = searchspace_stats.objective_time_keys
-        split_time_per_feval = np.empty((len(objective_time_keys), average_index_at_feval.shape[0]))
-        for key_index, key in enumerate(objective_time_keys):
-            split_time_per_feval[key_index] = searchspace_stats.objective_times_array[key_index, average_index_at_feval]
+        num_keys = len(objective_time_keys)
+        num_repeats = matching_indices_mask.shape[1]
+        masked_time_per_key = np.full((num_keys, matching_indices_mask.shape[0], num_repeats), np.NaN)
+
+        # for each key, apply the boolean mask
+        for key_index in range(num_keys):
+            masked_time_per_key[key_index, matching_indices_mask] = self._x_time_per_key[key_index, matching_indices_mask]
+
+        # remove where every repeat has NaN
+        time_in_range_per_key = np.full((num_keys, fevals_range.shape[0], num_repeats), np.NaN)
+        for key_index in range(num_keys):
+            all_nan_mask = ~np.all(np.isnan(masked_time_per_key[key_index]), axis=1)
+            time_in_range_per_key[key_index] = masked_time_per_key[key_index][all_nan_mask]
+
+        # get the median time per key at each repeat
+        split_time_per_feval = np.empty((num_keys, fevals_range.shape[0]))
+        for key_index in range(num_keys):
+            split_time_per_feval[key_index] = np.mean(time_in_range_per_key[key_index], axis=1)
+        assert split_time_per_feval.shape == (num_keys, fevals_range.shape[0]), f"{split_time_per_feval.shape} != {(num_keys, fevals_range.shape[0])}"
 
         return split_time_per_feval
 
     def get_split_times_at_time(self, time_range: np.ndarray, searchspace_stats: SearchspaceStatistics) -> np.ndarray:
-        times, values, times_1D, values_1D, _ = self._get_curve_over_time_values_in_range(time_range)
+        # get the raw times
+        nan_mask = ~np.isnan(self._x_time)
+        times_total = self._x_time[nan_mask]
+        times_split = self._x_time_per_key[:, nan_mask]
 
-        # get the index in searchspace at each value
-        searchspace_indices = get_indices_in_array(values_1D, searchspace_stats.objective_performances_total)
-
-        # for each time key, obtain the time at the times in time_range
-        objective_time_keys = searchspace_stats.objective_time_keys    # TODO use _x_time_per_key instead
-        split_time_per_timestamp = np.full((len(objective_time_keys), time_range.shape[0]), np.NaN)
-        for key_index, key in enumerate(objective_time_keys):
-            nan_mask = ~np.isnan(searchspace_indices)
-            time_1D = searchspace_stats.objective_times_array[key_index, searchspace_indices[nan_mask].astype(int)]
-            split_time_per_timestamp[key_index] = np.interp(time_range, times_1D[nan_mask], time_1D)
+        # for each key, interpolate the split times to the time range
+        num_keys = len(searchspace_stats.objective_time_keys)
+        split_time_per_timestamp = np.full((num_keys, time_range.shape[0]), np.NaN)
+        for key_index in range(num_keys):
+            # remove NaN
+            times_split_key = times_split[key_index]
+            nan_mask = ~np.isnan(times_split_key)
+            times_total_key = times_total[nan_mask]
+            times_split_key = times_split_key[nan_mask]
+            assert times_total_key.shape == times_split_key.shape
+            # interpolate the times to the time range
+            split_time_per_timestamp[key_index] = np.interp(time_range, times_total_key, times_split_key)
 
         return split_time_per_timestamp
 

autotuning_methodology/runner.py
@@ -155,8 +155,8 @@ def write_results(repeated_results: list, results_description: ResultsDescriptio
     objective_performance_results = get_nan_array()
     objective_performance_best_results = get_nan_array()
     objective_performance_stds = get_nan_array()
-    objective_time_results_per_key = get_nan_array()
-    objective_performance_results_per_key = get_nan_array()
+    objective_time_results_per_key = np.full((len(objective_time_keys), max_num_evals, len(repeated_results)), np.nan)
+    objective_performance_results_per_key = np.full((len(objective_time_keys), max_num_evals, len(repeated_results)), np.nan)
 
     # combine the results
     opt_func = np.nanmin if results_description.minimization is True else np.nanmax
@@ -166,15 +166,23 @@ def write_results(repeated_results: list, results_description: ResultsDescriptio
         objective_performance_best = np.nan
         for evaluation_index, evaluation in enumerate(repeat):
 
+            # set the number of function evaluations
+            fevals_results[evaluation_index, repeat_index] = evaluation_index + 1    # number of function evaluations are counted from 1 instead of 0
+
+            # in case of an invalid config, there is nothing to be registered
+            if str(evaluation['time']) == 'InvalidConfig':
+                warnings.warn(f"InvalidConfig found, this should have been caught by the framework restrictions")
+                continue
+
             # obtain the objective time per key
             objective_times_list = list()
-            for key in objective_time_keys:
-                if key in evaluation:
-                    key_sum_result = sum_inner_iterables(evaluation[key], performance=False)
-                    if key_sum_result is not None and not is_invalid_objective_time(key_sum_result):
-                        key_sum_result = key_sum_result / 1000    # TODO this miliseconds to seconds conversion is specific to Kernel Tuner
-                        objective_time_results_per_key[evaluation_index, repeat_index] = key_sum_result
-                        objective_times_list.append(key_sum_result)
+            for key_index, key in enumerate(objective_time_keys):
+                assert key in evaluation, f"Objective time key {key} not in evaluation ({evaluation})"
+                key_sum_result = sum_inner_iterables(evaluation[key], performance=False)
+                if key_sum_result is not None and not is_invalid_objective_time(key_sum_result):
+                    key_sum_result = key_sum_result / 1000    # TODO this miliseconds to seconds conversion is specific to Kernel Tuner
+                    objective_time_results_per_key[key_index, evaluation_index, repeat_index] = key_sum_result
+                    objective_times_list.append(key_sum_result)
             # sum the objective times of the keys
             if len(objective_times_list) >= 1:
                 objective_time = sum(objective_times_list)
@@ -182,14 +190,14 @@ def write_results(repeated_results: list, results_description: ResultsDescriptio
                     cumulative_objective_time += objective_time
                     objective_time_results[evaluation_index, repeat_index] = cumulative_objective_time
 
-            # obtain the objective time per key
+            # obtain the objective performance per key
             objective_performances_list = list()
-            for key in objective_time_keys:
-                if key in evaluation:
-                    key_sum_result = sum_inner_iterables(evaluation[key], performance=True)
-                    if key_sum_result is not None and not is_invalid_objective_performance(key_sum_result):
-                        objective_performance_results_per_key[evaluation_index, repeat_index] = key_sum_result
-                        objective_performances_list.append(key_sum_result)
+            for key_index, key in enumerate(objective_performance_keys):
+                assert key in evaluation, f"Objective performance key {key} not in evaluation ({evaluation})"
+                key_sum_result = sum_inner_iterables(evaluation[key], performance=True)
+                if key_sum_result is not None and not is_invalid_objective_performance(key_sum_result):
+                    objective_performance_results_per_key[key_index, evaluation_index, repeat_index] = key_sum_result
+                    objective_performances_list.append(key_sum_result)
             # sum the objective performances of the keys
             if len(objective_performances_list) >= 1:
                 objective_performance = sum(objective_performances_list)
@@ -200,9 +208,6 @@ def write_results(repeated_results: list, results_description: ResultsDescriptio
             # set the best objective performance
             if not is_invalid_objective_performance(objective_performance_best):
                 objective_performance_best_results[evaluation_index, repeat_index] = objective_performance_best
-
-            # set the number of function evaluations
-            fevals_results[evaluation_index, repeat_index] = evaluation_index + 1    # number of function evaluations are counted from 1 instead of 0
 
     # write to file
     numpy_arrays = {